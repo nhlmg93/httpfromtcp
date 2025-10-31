1b3ae679a477ef6a62ed12402e0fdefa2e0cce15
Nathan Helmig <nhlmg93@gmail.com>
2025-10-31 10:40:54 -0500

    validating header name

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 type Headers map[string]string
@@ -12,19 +13,11 @@ const crlf = "\r\n"
 func NewHeaders() Headers {
 	return make(Headers)
 }
-
-func parseHeader(fieldLine []byte) (string, string, error) {
-	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
-	if len(parts) != 2 {
-		return "", "", fmt.Errorf("malformed field line")
-	}
-	name := parts[0]
-	value := bytes.TrimSpace(parts[1])
-
-	if bytes.HasSuffix(name, []byte(" ")) {
-		return "", "", fmt.Errorf("malformed field name")
-	}
-	return string(name), string(value), nil
+func (h Headers) Get(name string) string {
+	return h[strings.ToLower(name)]
+}
+func (h Headers) Set(name, value string) {
+	h[strings.ToLower(name)] = value
 }
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	n = 0
@@ -43,8 +36,45 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		if err != nil {
 			return 0, false, err
 		}
+
+		if !isValidToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
+
 		n += idx + len(crlf)
-		h[name] = value
+		h.Set(name, value)
 	}
 	return n, done, err
+}
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed field line")
+	}
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("malformed field name")
+	}
+	return string(name), string(value), nil
+}
+
+func isValidToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
 }
